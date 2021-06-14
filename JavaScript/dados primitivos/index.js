6ea6f3373ec9a94f7f6ec453def65348245f18b7
Rafael Castro <rafaelcastrobr@gmail.com>
2021-06-14 10:21:11 -0300

    creat two class

---
+++ JavaScript/dados primitivos/index.js
@@ -0,0 +1,8 @@
+let mundo;
+mundo = 4;
+mundo = 10;
+
+
+console.log(mundo);
+
+// String, number, undefined, null, boolean, symbol
--- JavaScript/variaveis/index.js
+++ JavaScript/variaveis/index.js
@@ -4,16 +4,15 @@ let nome = 'Rafael0';
 console.log(nome, 'Pereira');
 */
 
-const nome = 'Rafael';
-const sobrenome = 'Castro';
+const nome = 'Rafael Castro';
 const idade = 30;
 const peso = 90;
-const altura = 1.75;
-let imc = (peso / (altura * altura)).toFixed(1);
+const alturaEmM = 1.75;
+let imc = (peso / (alturaEmM * alturaEmM)).toFixed(1);
 let anoNascimento = 2021 - idade;
 
-console.log(nome, sobrenome, 'tem', idade, 'anos,', 'pesa', peso, 'Kg.');
-console.log('Tem', altura, 'de altura e', 'seu IMC é de', imc);
-console.log(nome, sobrenome, 'nasceu em', anoNascimento)
+console.log(`${nome} tem ${idade} anos, pesa ${peso} Kg`);
+console.log(`Tem ${alturaEmM} de altura e seu IMC é de ${imc}`);
+console.log(`${nome} nasceu em ${anoNascimento}`)
 
 
