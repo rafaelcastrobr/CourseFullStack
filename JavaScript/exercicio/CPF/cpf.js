b7a367f8ce739cdb3b1481d179a340aeb870b37f
Rafael Castro <rafaelcastrobr@gmail.com>
2021-07-05 19:00:48 -0300

    cpf validator

--- JavaScript/exercicio/CPF/cpf.js
+++ JavaScript/exercicio/CPF/cpf.js
@@ -1,35 +1,63 @@
-/**
- * [x] Pega o cpf tira os dois utimos numeros
- * [x] separa em array 
- * [] multiplicar primeira vez
- * [] somar primeira vez
- * [] criar digito
- * [] adicionar digito
- * [] multiplicar segunda vez
- * [] somar segunda vez
- * [] criar segundo digito
- * [] comparar
- */
+// 705.484.450-52 070.987.720-03
+/*
+7x  0x 5x 4x 8x 4x 4x 5x 0x
+10  9  8  7  6  5  4  3  2
+70  0  40 28 48 20 16 15 0 = 237
 
-let cpf = '111.444.777-35';
-let cpfLimpo = cpf.replace(/\D+/g, '');
-let cpfSemDig = cpfLimpo.slice(0,-2);
-let cpfArray = Array.from(cpfSemDig);
+11 - (237 % 11) = 5 (Primeiro dígito)
+Se o número digito for maior que 9, consideramos 0.
 
-let cpfMultiplica1R = [];
-const cpfMultiplica1 = cpfArray.map(function (valor) {
-  for (let i = 2; i > valor;i++) {
-    let resultado = i * valor;
-    if (resultado > 9) {
-      resultado = 0;
-      cpfMultiplica1R = resultado;
-    } 
-    if (resultado <= 9) {
-     cpfMultiplica1R = resultado;
+7x  0x 5x 4x 8x 4x 4x 5x 0x 5x
+11 10  9  8  7  6  5  4  3  2
+77  0  45 32 56 24 20 20 0  10 = 284
+
+11 - (284 % 11) = 2 (Primeiro dígito)
+Se o número digito for maior que 9, consideramos 0.
+*/
+
+
+function ValidaCPF(cpfEnviado) {
+  Object.defineProperty(this, 'cpfLimpo', {
+    enumerable: true,
+    get: function() {
+      return cpfEnviado.replace(/\D+/g, '');
     }
+  });
+}
+
+ValidaCPF.prototype.valida = function() {
+  if(typeof this.cpfLimpo === 'undefined') return false;
+  if(this.cpfLimpo.length !== 11) return false;
+  if(this.isSequence()) return false;
+
+
+  const cpfParcial = this.cpfLimpo.slice(0, -2);
+  const digito1 = this.criaDigito(cpfParcial);
+  const digito2 = this.criaDigito(cpfParcial + digito1);
+
+  const novoCpf = cpfParcial + digito1 + digito2;
+  return novoCpf === this.cpfLimpo;
+};
+
+ValidaCPF.prototype.criaDigito = function (cpfParcial) {
+  const cpfArray = Array.from(cpfParcial);
+
+  let regressivo = cpfArray.length+1;
+  const total = cpfArray.reduce((ac, val) => {
+    ac += (regressivo * Number(val));
+    regressivo--;
+    return ac;
+  }, 0);
   
-  };
+  const digito  = 11 - (total % 11);
+  return digito > 9 ? '0' : String(digito);
+}
 
-});
+ValidaCPF.prototype.isSequence = function() {
+  const sequencia  = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+  return sequencia === this.cpfLimpo;
+}
 
- console.log(cpfMultiplica1);
+
+const cpf = new ValidaCPF('407.554.208-45');
+console.log(cpf.valida());
