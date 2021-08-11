417684875acd5e5703eea4bdb0c03a2471a4e0fd
rafaelcastrobr <rafaelcastrobr@gmail.com>
2021-08-11 10:32:21 -0300

    update for

--- JavaScript/aula/for.js
+++ JavaScript/aula/for.js
@@ -2,16 +2,27 @@
 // For in - Retorna o índice ou chave (string, array ou objetos)
 // For of - Retorna  o valor em si (interáveis, arrays ou strings)
 
-const pessoas = ['joao', 'maria'];
+const pessoas = ['joao', 'maria', 'junior'];
 
-for (let i in pessoas) {
-  console.log(pessoas[i])
+for (let pos in pessoas) {
+  let cav = pessoas[pos];
+  //console.log(cav)
+};
+
+const pessoa = {
+  nome: 'Luiz', 
+  sobrenome: 'Otavio',
+  idade: 30
+};
+
+for(let pos in pessoa) {
+  console.log(pos, pessoa[pos])
 }
 
-console.log('##')
+// console.log('##')
 
 
 // com objetos não funciona
 for (let i of pessoas) {
-  console.log(i);
+  // console.log(i);
 }
