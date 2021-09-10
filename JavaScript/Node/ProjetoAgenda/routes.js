b3a0e2642cc0f4f7c60bfe92adade330c2fd7a67
rafaelcastrobr <rafaelcastrobr@gmail.com>
2021-09-10 18:40:04 -0300

    crar um agenda

--- JavaScript/Node/ProjetoAgenda/routes.js
+++ JavaScript/Node/ProjetoAgenda/routes.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const route = express.Router();
+
 const homeController = require('./src/controllers/homeController');
-const contatoController = require('./src/controllers/contatoController')
+const loginController = require('./src/controllers/loginController');
 
 
 //  rotas da home
-route.get('/', homeController.paginaInicial);
-route.post('/', homeController.trataPost)
+route.get('/', homeController.index);
 
-
-// rotas de contato
-route.get('/contato', contatoController.paginaInicial);
+// Rotas de login
+route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout)
 
 
 
--- JavaScript/Node/ProjetoAgenda/src/controllers/homeController.js
+++ JavaScript/Node/ProjetoAgenda/src/controllers/homeController.js
@@ -1,13 +1,5 @@
 
-exports.paginaInicial = (req, res, ) => {
-  res.render('index', {
-    titulo: 'Este sera o título',
-    numeros: [0,1,2,3,4,5,6,7,8,9]
-  });
-  return;
-}
+exports.index = (req, res) => {
+  res.render('index')
+};
 
-exports.trataPost = (req, res, ) => {
-  res.send(req.body);
-  return;
-}
